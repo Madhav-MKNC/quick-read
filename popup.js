0b56b6d078c645586bf4ee756469c2c56c785d21
Madhav Kumar <78339236+Madhav-MKNC@users.noreply.github.com>
2023-10-18 23:39:57 +0530

    popup

--- popup.js
+++ popup.js
@@ -1,1 +1,5 @@
+const toggleButton = document.getElementById("toggleButton");
 
+toggleButton.addEventListener("change", function () {
+  chrome.runtime.sendMessage({ enabled: toggleButton.checked });
+});
