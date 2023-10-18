54aa085b288379248ef26ab75cd1718b70b7d3f8
Madhav <Madhavampire@gmail.com>
2023-10-19 00:28:37 +0530

    manifest.json thik krdi

--- popup.ts
+++ popup.ts
@@ -7,9 +7,12 @@ toggleButton.addEventListener("change", () => {
   isEnabled = toggleButton.checked;
   chrome.runtime.sendMessage({ enabled: isEnabled });
 
+  // Execute content script when the toggle is enabled
   if (isEnabled) {
-    // Execute the conversion logic
-    convertToBionicReadable();
+    chrome.scripting.executeScript({
+      target: { tabId: sender.tab.id },
+      function: convertToBionicReadable,
+    });
 
     // After conversion, revert the toggle to "off"
     toggleButton.checked = false;
